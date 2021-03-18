9e8ffc71a90a917afefbf630ce15c348937333f9
Mert <mertturkmenoglu99@gmail.com>
2021-03-18 21:39:47 +0300

    Add worker function

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
+const { default: axios } = require('axios');
 const cron = require('node-cron');
 
-// Second (first star) (optional)
-// minute
-// hour
-// day of month
-// month
-// year (last star)
-cron.schedule('* * * * * *', () => {
-  console.log('Running task');
+const API = "http://www.randomnumberapi.com/api/v1.0/random?min=0&max=20&count=1"
+let lastNumber = undefined;
+
+console.log('Application started:', new Date().toISOString());
+
+cron.schedule('* * * * *', () => {
+  console.log('New task:', new Date().toISOString());
+  console.log('Last number was:', lastNumber);
+
+  axios.get(API)
+    .then(res => {
+      const num = res.data[0]
+      console.log('New number:', num);
+      lastNumber = num;
+    })
+    .catch(err => console.error(err));
 });
